4d2f453ff4a9b473b806564d2617dce4c15782ce
gwozdziu1017 <gwozdziu1017@o2.pl>
2025-09-17 23:09:15 +0200

    initial commit

---
+++ MoodChecker/MoodCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodCheckerApp.swift
+//  MoodChecker
+//
+//  Created by Damian Gwóźdź on 17/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
